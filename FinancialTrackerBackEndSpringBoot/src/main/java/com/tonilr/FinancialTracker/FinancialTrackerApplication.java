fbf1020642f016d7ee82a89b7c67b7a0f44e43d7
ToniLR999 <tonilupianezroman@gmail.com>
2024-08-04 11:32:32 +0200

    Spring boot project

---
+++ FinancialTrackerBackEndSpringBoot/src/main/java/com/tonilr/FinancialTracker/FinancialTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tonilr.FinancialTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTrackerApplication.class, args);
+	}
+
+}
---
+++ FinancialTrackerBackEndSpringBoot/src/test/java/com/tonilr/FinancialTracker/FinancialTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonilr.FinancialTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
