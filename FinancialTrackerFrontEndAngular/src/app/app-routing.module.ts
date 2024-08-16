fec5325d0e7069671652beafbdb92a834f7b5d41
ToniLR999 <tonilupianezroman@gmail.com>
2024-08-16 18:10:06 +0200

    Home structure and path

--- FinancialTrackerFrontEndAngular/src/app/app-routing.module.ts
+++ FinancialTrackerFrontEndAngular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component: HomeComponent},
+  { path: '', redirectTo: '/Home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
