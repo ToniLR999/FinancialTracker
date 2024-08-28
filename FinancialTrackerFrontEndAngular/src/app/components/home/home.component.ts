ae9150013ef5bad6e3b00e0940f6e9b0fe64d59e
Toni <tonilupianezroman@gmail.com>
2024-08-28 21:40:45 +0200

    Card carousel

--- FinancialTrackerFrontEndAngular/src/app/components/home/home.component.ts
+++ FinancialTrackerFrontEndAngular/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild, OnInit, Renderer2 } from '@angular/core';
 import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, LineController } from 'chart.js';
 
 
@@ -10,9 +10,18 @@ import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, To
 })
 export class HomeComponent  implements OnInit, AfterViewInit  {
   @ViewChild('myChart', { static: false }) myChart!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef<HTMLDivElement>;
+  @ViewChild('wrapper', { static: false }) wrapper!: ElementRef<HTMLDivElement>;
+  @ViewChild('arrowLeft', { static: false }) arrowLeft!: ElementRef<HTMLDivElement>;
+  @ViewChild('arrowRight', { static: false }) arrowRight!: ElementRef<HTMLDivElement>;
 
+  private firstCardWidth!: number;
+  private isDragging = false;
+  private startX!: number;
+  private startScrollLeft!: number;
+  private timeoutId: any;
 
-  constructor(){
+  constructor(private renderer: Renderer2){
     Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, LineController);
 
 
@@ -24,10 +33,27 @@ export class HomeComponent  implements OnInit, AfterViewInit  {
   ngAfterViewInit(): void {
     const canvas = this.myChart.nativeElement;
     const ctx = canvas.getContext('2d');
-  
+
     console.log('ViewChild:', this.myChart, ctx); // Debería mostrar información sobre el canvas
-  
     
+    const carousel = this.carousel.nativeElement;
+    const wrapper = this.wrapper.nativeElement;
+    const firstCard = carousel.querySelector(".card") as HTMLElement;
+    this.firstCardWidth = firstCard.offsetWidth;
+    
+    // Event listeners
+    this.renderer.listen(carousel, 'mousedown', this.dragStart.bind(this));
+    this.renderer.listen(carousel, 'mousemove', this.dragging.bind(this));
+    this.renderer.listen(document, 'mouseup', this.dragStop.bind(this));
+    this.renderer.listen(wrapper, 'mouseenter', () => clearTimeout(this.timeoutId));
+    this.renderer.listen(wrapper, 'mouseleave', this.autoPlay.bind(this));
+
+    // Arrow buttons event listeners
+    this.renderer.listen(this.arrowLeft.nativeElement, 'click', () => this.scrollCarousel(-this.firstCardWidth));
+    this.renderer.listen(this.arrowRight.nativeElement, 'click', () => this.scrollCarousel(this.firstCardWidth));
+
+    this.autoPlay();
+
     if (ctx) {
       new Chart(ctx, {
         type: 'line',
@@ -83,6 +109,56 @@ export class HomeComponent  implements OnInit, AfterViewInit  {
       });
     } else {
       console.error('No se pudo obtener el contexto 2D del canvas');
-    }  }
+    }  
+  
+  
+  
+  
+  }
+
+  private dragStart(e: MouseEvent) {
+    this.isDragging = true;
+    this.renderer.addClass(this.carousel.nativeElement, 'dragging');
+    this.startX = e.pageX;
+    this.startScrollLeft = this.carousel.nativeElement.scrollLeft;
+  }
+
+  private dragging(e: MouseEvent) {
+    if (!this.isDragging) return;
+
+    const carousel = this.carousel.nativeElement;
+    const newScrollLeft = this.startScrollLeft - (e.pageX - this.startX);
+
+    if (newScrollLeft <= 0 || newScrollLeft >= carousel.scrollWidth - carousel.offsetWidth) {
+      this.isDragging = false;
+      return;
+    }
+
+    carousel.scrollLeft = newScrollLeft;
+  }
+
+  private dragStop() {
+    this.isDragging = false;
+    this.renderer.removeClass(this.carousel.nativeElement, 'dragging');
+  }
+
+  private autoPlay() {
+    if (window.innerWidth < 800) return;
+
+    const carousel = this.carousel.nativeElement;
+    const totalCardWidth = carousel.scrollWidth;
+    const maxScrollLeft = totalCardWidth - carousel.offsetWidth;
+
+    if (carousel.scrollLeft >= maxScrollLeft) return;
+
+    this.timeoutId = setTimeout(() => {
+      carousel.scrollLeft += this.firstCardWidth;
+      this.autoPlay();
+    }, 2500);
+  }
+
+  private scrollCarousel(scrollAmount: number) {
+    this.carousel.nativeElement.scrollLeft += scrollAmount;
+  }
 
 }
