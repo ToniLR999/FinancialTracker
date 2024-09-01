ce116ba27a913b749e0135a703d7b3bc2288b51f
Toni <tonilupianezroman@gmail.com>
2024-09-01 19:39:02 +0200

    Login and sign up screens

--- FinancialTrackerFrontEndAngular/src/app/app-routing.module.ts
+++ FinancialTrackerFrontEndAngular/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MyWalletsComponent } from './components/my-wallets/my-wallets.component';
 import { PaymentsComponent } from './components/payments/payments.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogINComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
   {path: 'Home', component: HomeComponent},
   {path: 'MyWallets', component: MyWalletsComponent},
+  {path: 'LogIn', component: LogINComponent},
+  {path: 'SignUp', component: SignUpComponent},
   {path: 'Payments', component: PaymentsComponent},
   { path:  '**', redirectTo: '/Home', pathMatch: 'full' }
 ];
